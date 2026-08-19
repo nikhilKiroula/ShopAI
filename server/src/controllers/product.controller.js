7b0c902a4f11ac8d95ce049cea131c2e9d10e352
Nikhil Singh Kiroula <nikhilkiroula7@gmail.com>
2026-08-19 11:48:31 +0530

    feat: implement product retrieval and management routes with corresponding controller functions

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -38,5 +38,110 @@ const createProduct = asyncHandler(async (req, res) => {
         ))
 });
 
+const getAllProducts = asyncHandler(async (req, res) => {
 
-export { createProduct }
+    const products = await Product.find({ isActive: true });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            products,
+            "All Products Fetched Successfully"
+        ));
+});
+
+const getProductById = asyncHandler(async (req, res) => {
+
+    const { productId } = req.params;
+
+    const product = await Product.findOne({
+        _id: productId,
+        isActive: true,
+    });
+
+    if (!product) throw new ApiError(404, "Product not found");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            product,
+            "Product fetched successfully"
+        ))
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+
+    const { productId } = req.params;
+
+    const {
+        name,
+        description,
+        price,
+        category,
+        brand,
+        images,
+        stock,
+        isActive,
+    } = req.body;
+
+    const product = await Product.findById(productId);
+
+    if (!product) throw new ApiError(404, "Product not found");
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+        productId,
+        {
+            $set: {
+                name,
+                description,
+                price,
+                category,
+                brand,
+                images,
+                stock,
+                isActive,
+            },
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            updatedProduct,
+            "Product updated successfully"
+        ))
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+
+    const { productId } = req.params;
+
+    const product = await Product.findById(productId);
+
+    if (!product) throw new ApiError(404, "Product not found");
+
+    await Product.findByIdAndDelete(productId);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            null,
+            "Product deleted successfully"
+        ))
+});
+
+export {
+    createProduct,
+    getAllProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct,
+}
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-
-import { createProduct } from "../controllers/product.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+import {
+    createProduct,
+    deleteProduct,
+    getAllProducts,
+    getProductById,
+    updateProduct,
+} from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.post("/", verifyJWT, createProduct);
+router.get("/", getAllProducts);
+router.get("/:productId", getProductById);
+router.patch("/:productId", verifyJWT, updateProduct);
+router.delete("/:productId", verifyJWT, deleteProduct);
 
-export default router;
+export default router; 
