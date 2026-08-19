db8f512d7eab91184b13f93ecd13f620a54b57cf
Nikhil Singh Kiroula <nikhilkiroula7@gmail.com>
2026-08-19 10:13:02 +0530

    feat: add product model with schema definition for e-commerce functionality

---
+++ server/src/models/product.model.js
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+    {
+
+        name:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+
+        description:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+
+        price:{
+            type:Number,
+            required:true,
+            min:0,
+        },
+
+        category:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+
+        brand:{
+            type:String,
+            trim:true,
+        },
+
+        images:[
+            {
+                type:String,
+            },
+        ],
+
+        stock:{
+            type:Number,
+            required:true,
+            min:0,
+            default:0,
+        },
+
+        ratings:{
+            type:Number,
+            max:5,
+            min:0,
+            default:0,
+        },
+
+        isActive:{
+            type:Boolean,
+            default:true,
+        },
+
+        createdBy:{
+            type:mongoose.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+    },
+    { timestamps: true });
+
+export const Product = mongoose.model("Product", productSchema);
