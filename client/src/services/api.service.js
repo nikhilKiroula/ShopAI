dc15cf3db25b895381a8de3cabd5b4b8719a93a2
Nikhil Singh Kiroula <nikhilkiroula7@gmail.com>
2026-08-26 19:05:07 +0530

    fix: update CORS origin to production URL

--- client/src/services/api.service.js
+++ client/src/services/api.service.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const api = axios.create(
     {
-        baseURL: "http://localhost:5000/api",
+        baseURL: "https://shopai-h4bb.onrender.com",
         withCredentials: true,
     }
 );
 
-export default api;
+export default api; 
